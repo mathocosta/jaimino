3bde61bebc98284991d9e8b8e543d8ac2ab38e12
Matheus Costa <matheusoliveiracos@gmail.com>
2017-12-07 17:22:04 -0300

    Adding a parser to fix the bug of incomplete data

--- arduino/server.js
+++ arduino/server.js
@@ -1,15 +1,20 @@
 const path = require('path')
 let Serialport = require('serialport')
+const Readline = Serialport.parsers.Readline
+
 const FileSync = require('lowdb/adapters/FileSync')
 const db = require('../db/db')
 
 db.initDB(new FileSync(path.join(__dirname, '..', 'db/db.json')))
 
-if (process.env.NODE_ENV == 'dev') { // Para testes
+if (process.env.NODE_ENV == 'dev') { // Para testes sem o Arduino 
   Serialport = require('virtual-serialport')
 }
 
-let port = new Serialport("COM3", { baudRate: 9600 })
+const parser = new Readline({ delimiter: '\r\n' })
+const port = new Serialport("COM3", { baudRate: 9600 })
+
+port.pipe(parser)
 
 port.on('open', (err) => {
   console.log(`> ${__filename} Port open`)
@@ -19,9 +24,14 @@ port.on('open', (err) => {
 
 // Quando qualquer dado vem do Arduino é feito o comando baseando-se primeiramente
 // no primeiro 'pedaço', que é o identificador do comando. Depois passa os dados para o index.
-port.on('data', data => {
+
+port.on('data', onData) // Para testar sem Arduino
+
+//parser.on('data', onData)
+
+function onData(data) {
   let data_slices = data.split(':')
-  
+
   // TODO: Colocar feedbacks para o Arduino
   switch (data_slices[0]) {
     // Entrada de um morador.
@@ -47,7 +57,7 @@ port.on('data', data => {
       break
   }
   console.log(`> ${__filename} From Arduino: ${data}`)
-})
+}
 
 // NOTE: Remover quando não precisar mais.
 module.exports = port
