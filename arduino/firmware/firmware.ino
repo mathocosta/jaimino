197eaf3f80c9498976875491bc24ea79c198ab4b
Matheus Costa <matheusoliveiracos@gmail.com>
2017-12-11 10:43:13 -0300

    Adding observations for development

--- arduino/firmware/firmware.ino
+++ arduino/firmware/firmware.ino
@@ -1,3 +1,16 @@
+/*
+ * Firmware para o Arduino
+ *
+ * Protocolo dos códigos de envio:
+ *  - 1º pedaço
+ *    - 0(A): leitura do cartão
+ *    - 1(B): verificação de código digitado
+ *  - 2º pedaço os dados restantes
+ * Protocolo dos dados recebidos:
+ *  - 1º pedaço: comando que está sendo respondido, baseado na lógica do protocolo anterior.
+ *  - 2º pedaço: true(1) ou false(0) se o comando deu certo.
+ */
+
 #include <Keypad.h>
 #include <SPI.h>
 #include <MFRC522.h>
@@ -9,8 +22,6 @@
 #define BUZZER 49
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-
-char st[20];
 
 const byte ROWS = 4;
 const byte COLS = 3; 
@@ -30,6 +41,7 @@ void setup() {
   Serial.begin(9600);
   pinMode(50, OUTPUT);
   pinMode(RELAY, OUTPUT);
+
   SPI.begin();        // Inicia SPI bus
   mfrc522.PCD_Init(); // Inicia MFRC522
 }
@@ -45,6 +57,7 @@ void cartaoLogic() {
   conteudo.toUpperCase();
   Serial.println(conteudo);
 
+  // TODO: Colocar essa parte na função de recebimento
   if (conteudo.substring(1) == "B3 72 6C 9A") {
     tone(BUZZER, 262, 500);
     digitalWrite(RELAY, HIGH);
@@ -62,6 +75,16 @@ void cartaoLogic() {
   }
 }
 
+// Função para gerenciar as respostas do computador
+void responseHandler() {
+
+}
+
+/*
+ * - Verificação dos digitos
+ * - Verificação do cartao
+ * - Função para as respostas do computador
+ */
 void loop() {
   char key = keypad.getKey();
   if (key) {
@@ -83,6 +106,5 @@ void loop() {
 
   if (mfrc522.PICC_IsNewCardPresent() && mfrc522.PICC_ReadCardSerial()) {
     cartaoLogic();
-    return;
   }
 }
